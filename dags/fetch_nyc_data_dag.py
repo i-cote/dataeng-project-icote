ac128851ccd5160c71d163b23883d0ab142236c8
Irvin Cote <irvincoteg@gmail.com>
2025-01-12 02:23:26 +0100

    Changing xlsx to json and filtering out entries with less that 94 people

--- dags/fetch_nyc_data_dag.py
+++ dags/fetch_nyc_data_dag.py
@@ -3,6 +3,7 @@ from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 import requests
 import json
+import pandas as pd
 
 # Define the script for fetching JSON data
 def fetch_and_log_nyc_data():
@@ -59,6 +60,28 @@ def fetch_csv_file():
         print(f"Error downloading file: {e}")
     except Exception as e:
         print(f"An error occurred: {e}")
+
+def convert_xlsx_to_json(**kwargs):
+
+    input_file = '/tmp/nyc_detailed_race_and_ethnicity_data_2020.xlsx'  # Path to the input Excel file
+
+    df = pd.read_excel(input_file, header=[0, 1, 2, 3])
+    df.columns = ['_'.join(col).strip() for col in df.columns.values]
+    # Convert to dictionary first
+    data_dict = df.to_dict(orient='records')
+
+    # Filter out entries with population below 94 000
+    filtered_data = [
+        entry for entry in data_dict
+        if entry.get('2020 Detailed Race and Ethnicity Groups, Alone or In Any Combination_Total Population_Number_Pop', 0) >= 800000
+    ]
+    # Convert to JSON string with custom formatting
+    json_string = json.dumps(filtered_data, indent=2)
+    # Save to file
+    with open('/opt/airflow/config/pop.json', 'w') as f:
+        f.write(json_string)
+
+
 
 # Define the script to derive GEOID for each arrest record
 def derive_geoid():
@@ -188,6 +211,12 @@ fetch_csv_task = PythonOperator(
     dag=dag
 )
 
+convert_xlsx_to_json_task = PythonOperator(
+    task_id='convert_xlsx_to_json',
+    python_callable=convert_xlsx_to_json,
+    dag=dag
+)
+
 derive_geoid_task = PythonOperator(
     task_id='derive_geoid',
     python_callable=derive_geoid,
@@ -202,4 +231,5 @@ validate_coordinates_task = PythonOperator(
 
 
 # Set task dependencies
-fetch_data_task >> validate_coordinates_task >> derive_geoid_task >> fetch_csv_task
+fetch_data_task >> validate_coordinates_task >> derive_geoid_task 
+fetch_csv_task >> convert_xlsx_to_json_task
