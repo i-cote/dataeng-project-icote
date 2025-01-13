004ee904ac4c8f49fc948cd8aa81e92ea071218a
Irvin Cote <irvincoteg@gmail.com>
2025-01-13 13:39:29 +0100

    Making multiple api calls with different offsets to fetch the arrest data to get various dates in the data

--- dags/tasks/fetch_arrest_data_from_api_task.py
+++ dags/tasks/fetch_arrest_data_from_api_task.py
@@ -4,7 +4,7 @@ import requests
 # Define the script for fetching JSON data
 def fetch_arrest_data_from_api():
     # API endpoint and parameters
-    API_ENDPOINT = "https://data.cityofnewyork.us/resource/8h9b-rp9u.json?$limit=1000"
+    API_ENDPOINT = "https://data.cityofnewyork.us/resource/8h9b-rp9u.json"
     API_TOKEN = "iAn5FiC7n6AnwsEyG8JNn9fYt"  # Replace with your API token if required
 
     # Headers for API request (include your token if necessary)
@@ -12,24 +12,26 @@ def fetch_arrest_data_from_api():
         "X-App-Token": API_TOKEN
     } if API_TOKEN else {}
 
-    try:
-        # Fetch the data from the API
-        response = requests.get(API_ENDPOINT, headers=headers)
-        response.raise_for_status()  # Raise an error for HTTP issues
+    offset_step = 600
+    limit = 3
+    max_calls = 50
+    aggregated_data = []
 
-        data = response.json()
+    for i in range(max_calls):
+        offset = i * offset_step
+        params = {
+            "$limit": limit,
+            "$offset": offset
+        }
 
-        if not data:
-            print("No data available from the API.")
-            return
+        response = requests.get(API_ENDPOINT,headers=headers, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            print(f"Call {i+1}: Fetched records {offset} to {offset + limit - 1}")
+            aggregated_data.extend(data)  # Aggregate the data
+        else:
+            print(f"Error: API call {i+1} failed with status code {response.status_code}")
 
-        # Save data for further processing
-        with open('/tmp/nyc_arrest_data.json', 'w') as f:
-            json.dump(data, f)
-
-        print('Arrest data successfully saved')
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data from the API: {e}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    # Write aggregated data to a JSON file
+    with open('/opt/airflow/config/nyc_arrest_data.json', 'w') as file:
+        json.dump(aggregated_data, file, indent=4)
